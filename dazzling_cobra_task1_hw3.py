61acd728ff3a6362465af3f2402522c04491d62e
Gisela Chodos <gisela.chodos@gmail.com>
2016-11-14 18:55:36 -0700

    task1 python file

---
+++ dazzling_cobra_task1_hw3.py
@@ -0,0 +1,26 @@
+"""
+Ugh, where to start...
+"""
+#!/usr/bin/env python
+import sys
+
+# Main function
+def main():
+    return
+
+
+if __name__ == '__main__':
+    # Call Main
+    main()
+
+	Exit(0)
+
+
+
+
+
+
+
+
+
+
