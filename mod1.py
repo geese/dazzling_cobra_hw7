e97bc8e6523ee918c7e2f0f32a18c20a25d9c070
Karl <karlmarble@mail.weber.edu>
2016-11-15 02:10:04 +0000

    Added documentation to mod1.py

--- mod1.py
+++ mod1.py
@@ -1,31 +1,43 @@
 """
+Module to control the sliding doors on a van.
+Args:
+    Input ctrl containing values for ctrl{}
+Returns:
+    Whether the left right both or no doors opened based on input file.
 """
 #!/usr/bin/env python3
 import sys
 
-def ctrl():
-    file = { "LD":0, "RD":0, "CL":0, "ML":0, "LI":0, "LO":0, "RI":0, "RO":0, "GS":"0" }
+def ctrl(file):
+    """
+    Module to control the sliding doors on a van.
+    Args:
+        Input ctrl containing values for ctrl{}
+    Returns:
+        Whether the left right both or no doors opened based on input file.
+    """
+    ctrl = { "LD":0, "RD":0, "CL":0, "ML":0, "LI":0, "LO":0, "RI":0, "RO":0, "GS":"0" }
     
     #Check to see if master lock is enabled and vehicle is in park
-    if file{"ML"} == 1 or file{"GS"} != "P":
+    if ctrl{"ML"} == 1 or ctrl{"GS"} != "P":
         print("Neither Door Opens.")
     #Doors open without child lock
-    elif file{"CL"} == 0:
-        if (file{"LD"} == 1 or file{"LI"} == 1 or file{"LO"} == 1) and (file{"RD"} == 1 or file{"RI"} == 1 or file{"RO"} == 1):
+    elif ctrl{"CL"} == 0:
+        if (ctrl{"LD"} == 1 or ctrl{"LI"} == 1 or ctrl{"LO"} == 1) and (ctrl{"RD"} == 1 or ctrl{"RI"} == 1 or ctrl{"RO"} == 1):
             print("Both Doors Open.")
-        elif (file{"RD"} == 1 or file{"RI"} == 1 or file{"RO"} == 1) and (file{"LD"} == 0 or file{"LI"} == 0 or file{"LO"} == 0):
+        elif (ctrl{"RD"} == 1 or ctrl{"RI"} == 1 or ctrl{"RO"} == 1) and (ctrl{"LD"} == 0 or ctrl{"LI"} == 0 or ctrl{"LO"} == 0):
             print("Right Door Opens.")
-        elif (file{"LD"} == 1 or file{"LI"} == 1 or file{"LO"} == 1) and (file{"RD"} == 0 or file{"RI"} == 0 or file{"RO"} == 0):
+        elif (ctrl{"LD"} == 1 or ctrl{"LI"} == 1 or ctrl{"LO"} == 1) and (ctrl{"RD"} == 0 or ctrl{"RI"} == 0 or ctrl{"RO"} == 0):
             print("Left Door Opens.")
         else:
             print("Neither Door Opens.")
     #doos open with child lock
-    elif file{"CL"} == 1:
-        if (file{"LD"} == 1 or file{"LO"} == 1) and (file{"RD"} == 1 or file{"RO"} == 1):
+    elif ctrl{"CL"} == 1:
+        if (ctrl{"LD"} == 1 or ctrl{"LO"} == 1) and (ctrl{"RD"} == 1 or ctrl{"RO"} == 1):
             print("Both Doors Open.")
-        elif (file{"RD"} == 1 or file{"RO"} == 1) and (file{"LD"} == 0 or file{"LO"} == 0):
+        elif (ctrl{"RD"} == 1 or ctrl{"RO"} == 1) and (ctrl{"LD"} == 0 or ctrl{"LO"} == 0):
             print("Right Door Opens.")
-        elif (file{"LD"} == 1 or file{"LO"} == 1) and (file{"RD"} == 0 or file{"RO"} == 0) :
+        elif (ctrl{"LD"} == 1 or ctrl{"LO"} == 1) and (ctrl{"RD"} == 0 or ctrl{"RO"} == 0) :
             print("Left Door Opens.")
         else:
             print("Neither Door Opens.")
