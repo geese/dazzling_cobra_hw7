8f49615c1e30f3bea211ed195baa7b1f10394db4
Gisela Chodos <gisela.chodos@gmail.com>
2016-11-14 20:04:22 -0700

    added function to build list of dicts with record values

--- dazzling_cobra_task1_hw3.py
+++ dazzling_cobra_task1_hw3.py
@@ -19,13 +19,30 @@ def getMinivanTestFile(url):
     with urlopen(url) as testFile:
         for line in testFile:
             line_dc = line.decode('utf-8').strip('\n')
-            record_list.append(line_dc.split(','))
+            record_list.append(line_dc.split(',')[1:])
     return record_list
+
+
+def makeInputValuesDicts(record_list):
+    # separate the header values from the input values
+    header_record = record_list.pop(0)
+    values_dicts = []
+
+    for record in record_list:
+        value_dict = {}
+        index = 0
+        for value in record:
+            value_dict[header_record[index]] = value
+            index += 1
+        values_dicts.append(value_dict)
+    return values_dicts
 
 
 # Main function
 def main():
-    getMinivanTestFile('http://icarus.cs.weber.edu/~hvalle/cs3030/data/minivanTest.csv')
+    records = getMinivanTestFile('http://icarus.cs.weber.edu/~hvalle/cs3030/data/minivanTest.csv')
+    dicts = makeInputValuesDicts(records)
+    print(dicts)
     return
 
 
