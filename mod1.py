32be820d51fe4cb796b99816940d6107ed544e6f
locoslrak <karlmarble@mail.weber.edu>
2016-11-15 13:04:59 -0700

    Updated mod1
    change name of dict from ctrl{} to v{} and added validation line from *module1_hw7

--- mod1.py
+++ mod1.py
@@ -8,7 +8,7 @@ Returns:
 #!/usr/bin/env python3
 import sys
 
-def ctrl(file):
+def ctrl(values_dict):
     """
     Module to control the sliding doors on a van.
     Args:
@@ -16,28 +16,32 @@ def ctrl(file):
     Returns:
         Whether the left right both or no doors opened based on input file.
     """
-    ctrl = { "LD":0, "RD":0, "CL":0, "ML":0, "LI":0, "LO":0, "RI":0, "RO":0, "GS":"0" }
+    #v = { "LD":0, "RD":0, "CL":0, "ML":0, "LI":0, "LO":0, "RI":0, "RO":0, "GS":"0" }
+    v = values_dict
+    
+    if v['GS'] not in ['P','N','D','1','2','3','R']:
+        return "Invalid Record: Both doors stay closed."
     
     #Check to see if master lock is enabled and vehicle is in park
-    if ctrl{"ML"} == 1 or ctrl{"GS"} != "P":
+    if v{"ML"} == 1 or v{"GS"} != "P":
         print("Neither Door Opens.")
     #Doors open without child lock
-    elif ctrl{"CL"} == 0:
-        if (ctrl{"LD"} == 1 or ctrl{"LI"} == 1 or ctrl{"LO"} == 1) and (ctrl{"RD"} == 1 or ctrl{"RI"} == 1 or ctrl{"RO"} == 1):
+    elif v{"CL"} == 0:
+        if (v{"LD"} == 1 or v{"LI"} == 1 or v{"LO"} == 1) and (v{"RD"} == 1 or v{"RI"} == 1 or v{"RO"} == 1):
             print("Both Doors Open.")
-        elif (ctrl{"RD"} == 1 or ctrl{"RI"} == 1 or ctrl{"RO"} == 1) and (ctrl{"LD"} == 0 or ctrl{"LI"} == 0 or ctrl{"LO"} == 0):
+        elif (v{"RD"} == 1 or v{"RI"} == 1 or v{"RO"} == 1) and (v{"LD"} == 0 or v{"LI"} == 0 or v{"LO"} == 0):
             print("Right Door Opens.")
-        elif (ctrl{"LD"} == 1 or ctrl{"LI"} == 1 or ctrl{"LO"} == 1) and (ctrl{"RD"} == 0 or ctrl{"RI"} == 0 or ctrl{"RO"} == 0):
+        elif (v{"LD"} == 1 or v{"LI"} == 1 or v{"LO"} == 1) and (v{"RD"} == 0 or v{"RI"} == 0 or v{"RO"} == 0):
             print("Left Door Opens.")
         else:
             print("Neither Door Opens.")
     #doos open with child lock
-    elif ctrl{"CL"} == 1:
-        if (ctrl{"LD"} == 1 or ctrl{"LO"} == 1) and (ctrl{"RD"} == 1 or ctrl{"RO"} == 1):
+    elif v{"CL"} == 1:
+        if (v{"LD"} == 1 or v{"LO"} == 1) and (v{"RD"} == 1 or v{"RO"} == 1):
             print("Both Doors Open.")
-        elif (ctrl{"RD"} == 1 or ctrl{"RO"} == 1) and (ctrl{"LD"} == 0 or ctrl{"LO"} == 0):
+        elif (v{"RD"} == 1 or v{"RO"} == 1) and (v{"LD"} == 0 or v{"LO"} == 0):
             print("Right Door Opens.")
-        elif (ctrl{"LD"} == 1 or ctrl{"LO"} == 1) and (ctrl{"RD"} == 0 or ctrl{"RO"} == 0) :
+        elif (v{"LD"} == 1 or v{"LO"} == 1) and (v{"RD"} == 0 or v{"RO"} == 0) :
             print("Left Door Opens.")
         else:
             print("Neither Door Opens.")
