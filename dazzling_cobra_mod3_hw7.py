a2bfbaa9ceb87e25b64f0cb5c5f89e66cb355c70
Karl <karlmarble@mail.weber.edu>
2016-11-18 23:34:36 +0000

    changed names to hw7 per naming convention

---
+++ dazzling_cobra_mod3_hw7.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+Translate a decimal zipcode into a barcode.
+Args:
+    User will be asked to enter a zipcode.
+Returns:
+    Barcode format of user input zipcode.
+"""
+import sys
+
+
+barcode_table = {1:'00011', 2:'00101',3:'00110', 4:'01001', 5:'01010', 
+        6:'01100', 7:'10001', 8:'10010', 9:'10100', 0:'11000'}
+
+def printDigit():
+    """
+    Take zipcode from user input than prepare it for printing in barcode format.
+    Args: None
+    Returns:
+        zipcode
+    """
+    zip = list(input("Please input a zip code: "))
+    i = 0
+    for word in zip:
+        zip[i] = int(zip[i])
+        i += 1
+    chksum = sum(zip)
+    if chksum % 10 != 0:
+        rem = chksum % 10
+        zip.append(10 - rem)
+    else:
+        zip.append(0)
+    zipString = ''
+    for digit in zip:
+        zipString += str(digit)
+    print('zip: {}, check digit: {}'.format(zipString[0:-1], zipString[-1:]))
+    return zip
+
+
+def printBarCode(zip):
+    """
+    Prints decimal zipcode and check value as a barcode
+    using psuedo-binary. 
+    Output Format:
+        | xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx |
+        where x = either "|" = 1 or ":" = 0
+    Args:
+        Decimal zipcode with check number
+    Returns:
+        6 decimal digits expressed in psuedo-binary barcode
+    """
+    barcode = '|'
+    for word in zip:
+        for bit in barcode_table[word]:
+            barcode += ":" if bit == '0' else '|'
+    barcode += '|'
+    print(barcode)
+
+
+#main function
+def main():
+    """
+    Test function
+    """
+    #zip = input("Please input a zip code: ")
+    #printBarCode(zip)
+    zip = printDigit()
+    printBarCode(zip)
+
+if __name__ == "__main__":
+    #Call Main
+    main()
+
+    exit(0)
---
+++ dazzling_cobra_mod4_hw7.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+A program which takes zipcodes from an input file and converts the zipcodes
+into a barcode
+Displays the zipcode, the check digit, and the barcode
+"""
+import sys
+import dazzling_cobra_mod3_hw3 as dazzle
+from urllib.request import urlopen
+
+def help():
+    """
+    The usage is: dazzling_cobra_mod4_hw3
+    """
+
+def getZipCodes(url):
+    """
+    Decodes the information passed in by the url and strips the newline
+    Args:
+        A url which links to an input file
+    Returns:
+        Stripped/Decoded values
+    """
+    theZips = []
+    with urlopen(url) as theZipCodes:
+        for line in theZipCodes:
+            line_dc = line.decode('utf-8').strip('\n')
+            theZips.append(line_dc)
+    return theZips
+
+
+
+
+
+
+
+
+# Main function
+def main():
+    """
+    Takes the input file from the url and formats it into a bar code
+    using an imported function named dazzling_cobra_mod3_hw
+    Prints out the zipcode read from the input file, the check digit,
+    and also the bar code based off of the zipcode
+    """
+    zipList = getZipCodes('http://icarus.cs.weber.edu/~hvalle/cs3030/data/barCodeData.txt')
+    for zip in zipList:
+        nzip = list(zip)
+        i = 0
+        for word in nzip: 
+            nzip[i] = int(nzip[i])
+            i += 1
+        chksum = sum(nzip)
+        if chksum % 10 != 0:
+            rem = chksum % 10
+            nzip.append(10 - rem)
+        else:
+            nzip.append(0)
+        print("Please input a zip code: ",zip)
+        dazzle.printBarCode(nzip)
+    return
+
+
+if __name__ == '__main__':
+    # Call Main
+    main()
+    
+    exit(0)
