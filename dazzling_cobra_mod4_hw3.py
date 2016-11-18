9d0fc43f46db2e148ebfb26c6248b679fcc98af0
Jordan Newkirk <jordannewkirk23@gmail.com>
2016-11-17 23:21:36 -0700

    Added additional documentation

--- dazzling_cobra_mod4_hw3.py
+++ dazzling_cobra_mod4_hw3.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 """
-
+A program which takes zipcodes from an input file and converts the zipcodes
+into a barcode
+Displays the zipcode, the check digit, and the barcode
 """
 import sys
 import dazzling_cobra_mod3_hw3 as dazzle
 from urllib.request import urlopen
 
+def help():
+    """
+    The usage is: dazzling_cobra_mod4_hw3
+    """
 
 def getZipCodes(url):
+    """
+    Decodes the information passed in by the url and strips the newline
+    Args:
+        A url which links to an input file
+    Returns:
+        Stripped/Decoded values
+    """
     theZips = []
     with urlopen(url) as theZipCodes:
         for line in theZipCodes:
@@ -24,6 +37,12 @@ def getZipCodes(url):
 
 # Main function
 def main():
+    """
+    Takes the input file from the url and formats it into a bar code
+    using an imported function named dazzling_cobra_mod3_hw
+    Prints out the zipcode read from the input file, the check digit,
+    and also the bar code based off of the zipcode
+    """
     zips = getZipCodes('http://icarus.cs.weber.edu/~hvalle/cs3030/data/barCodeData.txt')
     for zip in zips:
         dazzle.printBarCode(zip)
